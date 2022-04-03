a02a64304786ae7afdd17e3e02bf0430779ef908
faysalcoder <faysalsikder378486@gmail.com>
2022-04-03 20:50:00 +0600

    Server Commit

---
+++ README.md
@@ -0,0 +1,55 @@
+# Bikezone Server
+# Bike Zone - Think Good
+## Live site Link:  https://bikezone-b0d4c.web.app/
+## It's using Email and Password or google sign in system
+### Project Detail: 
+1. This is a Bycicle Selling and Showroom website who works in the world.
+2. An user create his/ her own acount using Sign in systeme, It's controlled by Firebase Authentication System.
+3. An User Order an item at a time , but make sure user can be logged in . Otherwise user redirect in Login page.and and user Place order a product at a time submit his/her personal data.
+4. An user manage his Dashboard for Orders maintain Submit Review ,Pay etc.
+5. It has a admin role system .
+6. A admin can change products, add Products, Manage Orders, Make an user as an Admin etc role.
+
+## Uses: 
+HTML 
+CSS
+JS
+REACT
+NODE.js
+REACT ROUTER
+Metarial Ui
+BOOTSTRAP
+Heroku
+EXPRESS
+Mongodb
+
+
+
+## Component use:
+
+Home
+Header
+Tracking
+Banner
+Service
+Services
+Explore
+Footer
+Login
+Register
+Deshboard
+PlaceOrder
+Add Products
+My Order
+Manage All Orders
+Make Admin
+Manage Products
+Review
+Banner
+
+
+Backend data load from MongoDB.
+
+
+
+
---
+++ index.js
@@ -0,0 +1,222 @@
+const express = require('express')
+const app = express()
+const cors = require('cors');
+const { MongoClient } = require('mongodb')
+require('dotenv').config();
+const ObjectId = require('mongodb').ObjectId;
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@write-hub.bhr8q.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+async function run() {
+    try {
+        await client.connect();
+        const database = client.db('writeHub');
+        // const doctorsCollection = database.collection('');
+        const blogsCollection = database.collection('blogs');
+        const usersCollection = database.collection('users');
+        const commentsCollection = database.collection('comments');
+
+        // // doctor insert
+        // app.post('/doctors', async (req, res) => {
+        //     const doctor = req.body
+        //     const result = await doctorsCollection.insertOne(doctor);
+
+        //     res.json(result)
+        // })
+        // //appointment
+        // app.post('/placeappointment', async (req, res) => {
+        //     const appointment = req.body
+        //     const result = await appointmentsCollection.insertOne(appointment);
+
+        //     res.json(result)
+        // })
+
+
+        // app.get('/doctors', async (req, res) => {
+        //     const cursor = doctorsCollection.find({});
+        //     const page = req.query.page;
+        //     const size = parseInt(req.query.size);
+        //     let doctors;
+        //     const count = await cursor.count();
+
+        //     if (page) {
+        //         doctors = await cursor.skip(page * size).limit(size).toArray();
+        //     }
+        //     else {
+        //         doctors = await cursor.toArray();
+        //     }
+
+        //     res.send({
+        //         count,
+        //         doctors
+        //     })
+        // });
+        // //Reviews
+        // app.post('/reviews', async (req, res) => {
+        //     const review = req.body
+        //     const result = await reviewsCollection.insertOne(review);
+
+        //     res.json(result)
+        // })
+        // //Get doctors Data
+        // app.get('/doctors', async (req, res) => {
+        //     const cursor = doctorsCollection.find({});
+        //     const result = await cursor.toArray();
+        //     res.send(result)
+        // })
+        // // Get appointments Data
+        // app.get('/appointments', async (req, res) => {
+        //     const cursor = appointmentsCollection.find({});
+        //     const result = await cursor.toArray();
+        //     res.send(result)
+        // })
+        // //Get Review Data
+        // app.get('/reviews', async (req, res) => {
+        //     const cursor = reviewsCollection.find({});
+        //     const result = await cursor.toArray();
+        //     res.send(result)
+        // })
+        //Get Review Data
+        app.get('/users', async (req, res) => {
+            const cursor = usersCollection.find({});
+            const result = await cursor.toArray();
+            res.send(result)
+        })
+        // // Find Place appointment Data
+        // app.get('/placeappointment/:id', async (req, res) => {
+        //     const id = req.params.id
+
+        //     const query = { _id: ObjectId(id) }
+        //     const result = await doctorsCollection.findOne(query)
+        //     res.json(result)
+        // })
+        // //Admin Identify
+
+        app.get('/users/:email', async (req, res) => {
+            const email = req.params.email
+            const query = { email: email }
+            const user = await usersCollection.findOne(query)
+            let isAdmin = false
+            if (user?.role === 'Admin') {
+                isAdmin = true
+            }
+            console.log(isAdmin)
+            res.json({ admin: isAdmin })
+        })
+
+        app.post('/users', async (req, res) => {
+            const user = req.body
+            const result = await usersCollection.insertOne(user);
+
+            res.json(result)
+        })
+
+        app.put('/users', async (req, res) => {
+            const user = req.body
+
+            const filter = { email: user.email }
+            const option = { upsert: true }
+            const updateDoc = {
+                $set: {
+                    name: user.name,
+                    email: user.email
+                }
+            }
+            const result = await usersCollection.updateOne(filter, updateDoc, option)
+
+            res.json(result)
+        })
+
+        // app.put('/users/admin', async (req, res) => {
+        //     const user = req.body
+
+        //     const filter = { email: user.email }
+
+        //     const updateDoc = {
+        //         $set: {
+        //             role: 'Admin'
+        //         }
+        //     }
+        //     const result = await usersCollection.updateOne(filter, updateDoc)
+        //     res.json(result)
+        // })
+        // app.put('/doctors/review', async (req, res) => {
+        //     const data = req.body
+        //     console.log(data)
+        //     const filter = { name: data.doctor }
+
+        //     const updateDoc = {
+        //         $set: {
+        //             rating: data.rate
+        //         }
+        //     }
+        //     const result = await doctorsCollection.updateOne(filter, updateDoc)
+        //     res.json(result)
+        // })
+        // app.put('/appointments/confirm', async (req, res) => {
+
+        //     const { appointmentId } = req.body
+
+        //     const filter = { _id: ObjectId(appointmentId) }
+
+        //     const updateDoc = {
+        //         $set: {
+        //             status: true
+        //         }
+        //     }
+        //     const result = await appointmentsCollection.updateOne(filter, updateDoc)
+        //     res.json(result)
+        // })
+
+        // app.put('/reviews/confirm', async (req, res) => {
+
+        //     const { reviewId } = req.body
+
+        //     const filter = { _id: ObjectId(reviewId) }
+
+        //     const updateDoc = {
+        //         $set: {
+        //             status: true
+        //         }
+        //     }
+        //     const result = await reviewsCollection.updateOne(filter, updateDoc)
+        //     res.json(result)
+        // })
+
+        // app.delete('/doctors/:id', async (req, res) => {
+        //     const id = req.params.id;
+        //     const query = { _id: ObjectId(id) }
+        //     const result = await doctorsCollection.deleteOne(query)
+        //     res.json(result)
+        // })
+        // app.delete('/reviews/:id', async (req, res) => {
+        //     const id = req.params.id;
+        //     const query = { _id: ObjectId(id) }
+        //     const result = await reviewsCollection.deleteOne(query)
+        //     res.json(result)
+        // })
+        // app.delete('/appointments/:id', async (req, res) => {
+        //     const id = req.params.id;
+        //     const query = { _id: ObjectId(id) }
+        //     const result = await appointmentsCollection.deleteOne(query)
+        //     res.json(result)
+        // })
+
+    } finally {
+        //   await client.close();
+    }
+}
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
+    res.send('Server Running')
+})
+
+app.listen(port, () => {
+    console.log(`Listening ${port}`)
+})
