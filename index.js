dc92378ecf3891806033c5c5b5e4457cc9b66475
faysalcoder <faysalsikder378486@gmail.com>
2022-04-13 17:02:44 +0600

    blog delete

--- index.js
+++ index.js
@@ -188,12 +188,12 @@ async function run() {
         //     res.json(result)
         // })
 
-        // app.delete('/doctors/:id', async (req, res) => {
-        //     const id = req.params.id;
-        //     const query = { _id: ObjectId(id) }
-        //     const result = await doctorsCollection.deleteOne(query)
-        //     res.json(result)
-        // })
+        app.delete('/blogs/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) }
+            const result = await blogsCollection.deleteOne(query)
+            res.json(result)
+        })
         // app.delete('/reviews/:id', async (req, res) => {
         //     const id = req.params.id;
         //     const query = { _id: ObjectId(id) }
